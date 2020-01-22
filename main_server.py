37c165ce574139533e9aefedb61968419281052a
sagunkayastha <sgn.kayastha@gmail.com>
2020-01-22 11:51:21 +0545

    fixed printing

--- main_server.py
+++ main_server.py
@@ -149,13 +149,13 @@ model_ft = model_ft.cuda()
 criterion = nn.CrossEntropyLoss()
 
 # Observe that all parameters are being optimized
-optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+optimizer_ft = optim.Adam(model_ft.parameters(), lr=0.001)
 
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
 model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=25)
+                       num_epochs=50)
 
 # def run():
 #     torch.multiprocessing.freeze_support()
