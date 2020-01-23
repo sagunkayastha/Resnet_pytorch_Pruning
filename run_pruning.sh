3089ecd6ad3764b5332a7a7df1c232ba07aff312
sagunkayastha <sgn.kayastha@gmail.com>
2020-01-23 17:46:43 +0545

    l

--- run_pruning.sh
+++ run_pruning.sh
@@ -1,4 +1,4 @@
-until python3 main_server.py > training_log.log 2>&1 & disown; do
+until python3 main.py --dataset=rtvtr --model=resnet50 --data=../dataset2/ --pruning-method=22 --batch=32 --pruning_config=./configs/imagenet_resnet50_prune72.json --lr-decay-every=10 --momentum=0.9 --epochs=25 --pruning=False > training_log.log 2>&1 & disown; do
     echo "system crashed : error : $?, respawning... " &> crash.log
     sleep 1
 done
